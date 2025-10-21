a6366bd12fe5879fd64b15128163d81bee884089
Brayden Langley <brayden167@gmail.com>
2025-10-21 15:42:24 -0700

    New deps

--- src/index.ts
+++ src/index.ts
@@ -1927,7 +1927,7 @@ function generatePackageJson(backendDependencies: Record<string, string>) {
     license: 'ISC',
     dependencies: {
       ...backendDependencies,
-      '@bsv/overlay-express': '^0.7.14',
+      '@bsv/overlay-express': '^0.8.2',
       mysql2: '^3.11.5',
       tsx: '^4.19.2'
     },
